c8f56476a974a16528b84ae2f0b9c1bd6e89598e
Dirk Arnez <smalldirkalex@gmail.com>
2026-08-11 12:46:33 +0800

    - update local-run.sh

--- local-run.sh
+++ local-run.sh
@@ -11,4 +11,9 @@ qemu-system-aarch64 -m 1024 -smp 2 -cpu cortex-a57 -M virt -nographic \
 
 # \ -drive if=none,file=disk.img,id=hd0 -device virtio-blk-device,drive=hd0
 
+# to enable networking
+# -net nic,vlan=0 -net nic,vlan=1 -net user,vlan=1
+# 網卡 1 (eth0)：連接到 vlan=0 → 孤立網路（無外網，無其他設備）。
+# 網卡 2 (eth1)：連接到 vlan=1 → 連接 user 後端 → 可透過 NAT 訪問外網。
+
 read -p "done"
